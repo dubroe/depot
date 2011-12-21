323d77ae757587c385d42746da37a664a3aed656
Elan Dubrofsky <elan.dubrofsky@gmail.com>
2011-12-21 17:28:45 -0500

    Created carts and line items

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+    @counter = session[:counter] if session[:counter] > 5
   end
 
 end
